4b62fd14b6558ae8430ed6adf86e87ca22a6f544
James Oswald <jtoswald@albany.edu>
2023-05-03 07:50:54 -0400

    first steps in vampire integration

--- scripts/addSubmodules.sh
+++ scripts/addSubmodules.sh
@@ -1,10 +1,13 @@
 
-#This script is useful for every time I accidently mess up all the submodules
+#This script is useful for every time I accidently mess up all the submodules and need to redo them from scratch
 
 #	path = garden/vampire
 #	url = https://github.com/vprover/vampire.git
 #	shallow = true
 #	branch = 2d02e4655e1b08d1ca9ee7c0aade40f59f046460
 
-#Add vampire
+#Add vampire 4.7 as a shallow submodule
+cd ../garden
 git submodule add https://github.com/vprover/vampire.git 
+git submodule set-branch -b v4.7 garden/vampire
+git config -f ../.gitmodules submodule.garden/vampire.shallow true
