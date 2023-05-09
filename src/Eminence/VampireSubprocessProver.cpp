8d55ba526e0ba83c69b23287d8f607896b76a592
Brandon Rozek <brozek@brandonrozek.com>
2023-05-08 23:02:01 -0400

    First version of question answering

--- src/Eminence/VampireSubprocessProver.cpp
+++ src/Eminence/VampireSubprocessProver.cpp
@@ -1,7 +1,10 @@
 
 #include "VampireSubprocessProver.hpp"
+#include "../FormulaIO/writers.cpp"
 
 #include<filesystem>
+#include<numeric>
+#include<boost/format.hpp>
 #include<boost/filesystem.hpp>
 #include<boost/process.hpp>
 #include<boost/dll/runtime_symbol_info.hpp>
@@ -78,4 +81,164 @@ bool vamp::solve(const Problem& p){
             return false;
         }
     }
+}
+
+std::optional<vamp::answer> vamp::extractAnswer(
+    const Problem& p, const std::list<Term*>& vars, const vamp::answers& forbiddenAnswers) {
+    //Write the problem to the input file
+    static fs::path problemFilePath = fs::temp_directory_path()/problemFileName;
+    std::FILE* problemFile = std::fopen(problemFilePath.c_str(),"w");
+    if(!problemFile){
+        throw std::runtime_error("Failed to open proof temp file:\n" + problemFilePath.string());
+    }
+
+    /*
+        Rewriting the problem.
+
+        Since there's no way to output ?[X, Y, Z] in
+        the Formula to TPTP class, I'm providing this hacky
+        overlay method.
+
+        If you recursively nest (?[X]: ?[Y]: ?[Z]: ...)
+        then Vampire will only give an answer to X.
+
+        [1]: We have no way currently of grabbing back the original predicate
+        names and the link from makeLegalTPTP
+    */
+    std::string varListString = std::accumulate(
+         vars.begin(), vars.end(), std::string{},
+         [](const std::string& acc, Term* t) -> std::string {
+             return acc.empty() ? t->name : acc + ", " + t->name;
+         }
+     );
+
+    std::string tptpContents = boost::str(
+        boost::format("fof(goal, conjecture, ?[%1%]: (%2%))") %
+         varListString %
+         recursiveToTPTP(makeLegalTPTP(p.goal)) // TODO: [1]
+    );
+
+    // TODO: Need to include way to not allow forbidden answers within problem file
+
+
+    // BEGIN COPY PASTE
+    std::fwrite(tptpContents.c_str(), sizeof(char), tptpContents.length(), problemFile);
+    std::fclose(problemFile);
+
+
+    //run a vampire subprocess
+    bp::ipstream vampireOutput;
+    //We need to use search_path to force boost to look in the same directory as us
+    static bfs::path eminenceProverDir = bdll::program_location().parent_path();
+    static bfs::path vampireExecPath = bp::search_path("vampire", {eminenceProverDir}); 
+    int resultCode = bp::system(
+        vampireExecPath,
+        "--input_syntax", "tptp",
+        "-om", "szs",
+        "-av", "off", // EDIT MADE HERE
+        "-qa", "answer_literal", // EDIT MADE HERE
+        "--input_file",
+        problemFilePath.string(),
+        bp::std_out>vampireOutput
+    );
+
+    //Process vampire output
+    std::string resultString;
+    std::getline(vampireOutput, resultString, {});
+    //If vampire has exited with a non-zero exit code
+    if(resultCode != 0){
+        size_t incompleteStrat = resultString.find("incomplete strategy");
+        size_t userError = resultString.find("User error");
+        if(userError != std::string::npos){
+            ba::trim(resultString);
+            throw std::runtime_error("Vampire prover exited with a user error! Vampire output:\n" 
+                + resultString + "\nProblem Contents:\n" + tptpContents + "\n");
+        }else if(incompleteStrat != std::string::npos){
+            //Vampire was not able to find a proof due to an incomplete stratagy, return false
+            //since we are unable to solve it
+            return {};
+        }else{
+            ba::trim(resultString);
+            throw std::runtime_error("Vampire prover exited with an error! Vampire output:\n" 
+                + resultString + "\nProblem Contents:\n" + tptpContents + "\n");
+        }
+    }
+    
+    //Vampire exited sucessfully
+    std::string szsStatusString = "SZS status";
+    size_t szsStatusLocationStart = resultString.find(szsStatusString) + szsStatusString.length() + 1;
+    if(szsStatusLocationStart == std::string::npos){
+        ba::trim(resultString);
+        throw std::runtime_error(
+            "Vampire completed successfully with an unexpected result! Vampire output:\n" + resultString
+        );
+    }
+    size_t szsStatusLocationEnd = resultString.find(" ", szsStatusLocationStart);
+    std::string szsStatus = resultString.substr(szsStatusLocationStart, szsStatusLocationEnd-szsStatusLocationStart);
+        
+    // END COPY PASTE
+        
+    if (szsStatus != "Theorem" && szsStatus != "Tautology") {
+        return {};
+    }
+        
+    // Extract answer from string
+
+    // Example: % SZS answers Tuple [["two","one"]|_] for question_answer
+    std::string szsAnswerString = "SZS answers Tuple [[";
+    size_t szsAnswerLocationStart = resultString.find(szsAnswerString) + szsStatusString.length() + 1;
+        if(szsStatusLocationStart == std::string::npos){
+        ba::trim(resultString);
+        throw std::runtime_error(
+            "Vampire completed successfully with an unexpected result! Vampire output:\n" + resultString
+        );
+    }
+    size_t szsAnswerLocationEnd = resultString.find("]", szsAnswerLocationStart);
+    std::string szsAnswerStr = resultString.substr(szsAnswerLocationStart, szsAnswerLocationEnd-szsAnswerLocationStart);
+
+
+    // Split the input string into a term list
+    std::stringstream ss(szsAnswerStr);
+    std::string termStr;
+    std::list<Term*> szsAnswer;
+    while (std::getline(ss, termStr, ',')) {
+        szsAnswer.push_back(Const(termStr));
+    }
+
+    if (szsAnswer.size() != vars.size()) {
+        // TODO: Throw error
+        return {};
+    }
+
+    // Combine the answer and variable list to create a mapping
+    auto szsAnswerIt = szsAnswer.begin();
+    auto varsIt = vars.begin();
+    vamp::answer answer;
+    while (szsAnswerIt != szsAnswer.end() && varsIt != vars.end()) {
+        answer.push_back(std::make_pair(*szsAnswerIt, *varsIt));
+        ++szsAnswerIt;
+        ++varsIt;
+    }
+    
+    return answer;
+}
+
+std::optional<vamp::answers> vamp::extractAnswers(const Problem& p, const std::list<Term*>& vars) {
+    vamp::answers priorAnswers;
+
+    bool answerFound = true;
+    while (answerFound) {
+        std::optional<vamp::answer> ans = extractAnswer(p, vars, priorAnswers);
+        if (ans.has_value()) {
+            priorAnswers.push_back(ans.value());
+        } else {
+            answerFound = false;
+        }
+    }
+
+    if (priorAnswers.size() == 0) {
+        return {};
+    }
+
+    return priorAnswers;
 }
--- src/Eminence/VampireSubprocessProver.hpp
+++ src/Eminence/VampireSubprocessProver.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 #include<Problem/Problem.hpp>
+#include <optional>
 
 /**
  * @brief Functions for interacting with the Vampire Subprocess Prover
@@ -12,4 +13,15 @@ namespace vamp {
      * @return true if the problem is satisfiable and a proof has been found
     */
     bool solve(const Problem& problem);
+
+    using answer = std::list<std::pair<Term*, Term*>>;
+    using answers = std::list<answer>;
+
+    /**
+     * @param problem a first order problem
+     * @return An optional list of assignments if exists
+    */
+    std::optional<answer> extractAnswer(
+        const Problem& p, const std::list<Term*>& vars, const answers& forbiddenAnswers);
+    std::optional<answers> extractAnswers(const Problem& p, const std::list<Term*>& vars);
 }
