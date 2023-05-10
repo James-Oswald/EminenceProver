cbabd1baac5ae72eeb104415505015e1b37d091e
Brandon Rozek <brozek@brandonrozek.com>
2023-05-10 15:04:15 -0400

    Cleanup

--- src/Question/Question.cpp
+++ src/Question/Question.cpp
@@ -52,40 +52,55 @@ std::string Question::toFirstOrderTPTP() const {
         it's variables.
     */
 
-    std::string varListString = std::accumulate(
-         variablesToAnswer.begin(), variablesToAnswer.end(), std::string{},
-         [](const std::string& acc, Term* t) -> std::string {
-             return acc.empty() ? t->name : acc + ", " + t->name;
-         }
-    );
-
     std::string tptpContents = "";
 
+    // Write out all assumptions
     FormulaList::const_iterator itr = assumptions.begin();
     for(size_t i = 0; i < assumptions.size(); i++, itr++){
         std::string name = "assumption" + std::to_string(i);
         tptpContents += FormulaWriter::toFirstOrderTPTP(name, "axiom", *itr) + "\n\n";
     }
 
+
+    boost::format tptpFormat("fof(goal, question, ?[%1%]: (%2% %3%)).");
+
+    // %1%: String list of variable names
+    const std::string varListString = std::accumulate(
+         variablesToAnswer.begin(), variablesToAnswer.end(), std::string{},
+         [](const std::string& acc, Term* t) -> std::string {
+             return acc.empty() ? t->name : acc + ", " + t->name;
+         }
+    );
+
+    // %2%: TPTP Goal
+    const std::string tptpGoal = recursiveToTPTP(goal); // NOTE: [1] [2]
+
+
+    // %3%: Restrictions on answers
+    // Example string: ~((X = "a" & Y = "b" ) | (X = "b" & Y = "a"))
     std::string restrictionsStr = "";
 
     if (restrictions.size() > 0) {
 
+        boost::format restrictionsFormat("& ~(%1%)");
         std::string restrictionsSubStr = "";
-        auto restrictionsIt = restrictions.begin();
+        Question::Answers::const_iterator restrictionsIt = restrictions.begin();
 
-        for (size_t i = 0; i < restrictions.size(); i++, restrictionsIt++) {
-            const Question::Answer& restriction = *restrictionsIt;
-            auto restrictionIt = restriction.begin();
+        size_t i = 0;
+        for (const Question::Answer restriction: restrictions) {
+            Question::Answer::const_iterator restrictionIt = restriction.begin();
 
             // Establish the restriction (X = "something" & Y = "something2)"
             restrictionsSubStr += "(";
-            for (size_t j = 0; j < restriction.size(); j++, restrictionIt++) {
-                const auto &[variable, constant] = *restrictionIt;
+            size_t j = 0;
+            for (const auto &[variable, constant] : restriction) {
                 restrictionsSubStr += variable->name + " = " + constant->name;
+
+                // AND separating each assignment within a restriction
                 if (j != restriction.size() - 1) {
                     restrictionsSubStr += " & ";
                 }
+                j++;
             }
             restrictionsSubStr += ")";
 
@@ -93,19 +108,20 @@ std::string Question::toFirstOrderTPTP() const {
             if (i != restrictions.size() - 1) {
                 restrictionsSubStr += " | ";
             }
+            i++;
         }
 
         restrictionsStr += boost::str(
-            boost::format("& ~(%1%)") %
+            restrictionsFormat %
             restrictionsSubStr
         );
     }
 
     tptpContents += boost::str(
-        boost::format("fof(goal, question, ?[%1%]: (%2% %3%)).") %
-         varListString %
-         recursiveToTPTP(goal) % // TODO: [1] [2]
-         restrictionsStr
+        tptpFormat %
+        varListString %
+        tptpGoal % 
+        restrictionsStr
     );
 
     return tptpContents;
